6932dc1177d3722b13139ce1582d78fb7c3f8645
Ju Liu <ju.liu@welaika.com>
2016-12-19 19:19:30 +0000

    Finish up supervisors chapter

--- 08_supervisor/freq_overload.erl
+++ 08_supervisor/freq_overload.erl
@@ -2,7 +2,15 @@
 -export([start_link/0, add/2, delete/2]).
 -export([no_frequency/0, frequency_available/0, frequency_denied/0]).
 
-start_link() -> gen_event:start_link({local, ?MODULE}).
+start_link() ->
+    case gen_event:start_link({local, ?MODULE}) of
+	{ok, Pid} ->
+	    add(counters, {}),
+	    add(logger, {file, "log"}),
+	    {ok, Pid};
+	Error ->
+	    Error
+    end.
 
 no_frequency() -> gen_event:notify(?MODULE, {set_alarm, {no_frequency, self()}}).
 frequency_available() -> gen_event:notify(?MODULE, {clear_alarm, no_frequency}).
